e49cf8b0636b81600939dd284dd6aaba7eb4eb01
clement Roland <clementroland52@gmail.com>
2025-12-27 20:59:20 -0400

    Refactor Banner component to use CSS modules; remove old Banner.css file and update styles accordingly

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,14 @@
-import '../styles/Banner.css'
-
+import styles from'../styles/Banner.module.css'
+import logo from '../assets/electro.jpeg'
 const Banner = () =>{
     const tittle = '🌿 la maison jungle'
     return (
-    <div className="banner">
-        <h1 className="banner-tittle"> {tittle}</h1>
+    <div className={styles.banner}>
+        <img src={logo}
+        alt="Logo La maison jungle"
+        className="banner-logo"
+        />
+        <h1 className={styles.banner-tittle}> {tittle}</h1>
     </div>
     
 )
